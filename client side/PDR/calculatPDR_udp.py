4f7320dabede786158008a370dede9c8d2558533
Hamza Ait Baali <harticode@users.noreply.github.com>
2019-07-05 17:38:44 +0300

    Update calculatPDR_udp.py

--- client side/PDR/calculatPDR_udp.py
+++ client side/PDR/calculatPDR_udp.py
@@ -29,26 +29,6 @@ def usage():
     sys.exit(2)
 
 
-def server():
-    if len(sys.argv) > 2:
-        port = eval(sys.argv[2])
-    else:
-        port = MY_PORT
-    s = socket(AF_INET, SOCK_STREAM)
-    s.bind(('', port))
-    s.listen(1)
-    print 'Server ready...'
-    while 1:
-        conn, (host, remoteport) = s.accept()
-        while 1:
-            data = conn.recv(BUFSIZE)
-            if not data:
-                break
-            del data
-        conn.send('OK\n')
-        conn.close()
-        print 'Done with', host, 'port', remoteport
-
 
 def client():
     if len(sys.argv) < 4:
